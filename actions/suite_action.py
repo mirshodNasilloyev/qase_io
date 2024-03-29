528ba3132623504e07cfae70e2055b4310178d70
mirshodNasilloyev <mnasilloyev.ios@gmail.com>
2024-03-29 20:55:37 +0500

    error

--- actions/suite_action.py
+++ actions/suite_action.py
@@ -10,23 +10,15 @@ class SuiteAction(SuiteScreen):
         self.return_to_main_tab_with_close()
         return True
 
-    def check_suite_created(self, name):
+    def check_suite_created(self):
         self.click_tc_title()
-        time.sleep(10)
         self.click_cr_suite_btn()
-        time.sleep(2)
-        self.enter_suite_name(name)
-        time.sleep(2)
+        # self.enter_suite_name()
         self.click_create_confirm_btn()
-        time.sleep(5)
         assert self.is_created_msg_exist()
-        time.sleep(5)
         self.click_created_suite_title()
-        time.sleep(2)
         self.click_delete_icon()
-        time.sleep(2)
         self.click_confirm_delete_btn()
-        time.sleep(1)
         assert self.is_deleted_msg_exist()
-        time.sleep(5)
+        time.sleep(2)
         return True
--- screens/main_screen.py
+++ screens/main_screen.py
@@ -1,10 +1,8 @@
 from screens.base_screen import BaseScreen
 
-
-
-
 main_pg_title:str = "//*[contains(text(),\"All-in-one\")]"
 main_pg_login_btn:str = "//a[@id=\"signin\"]"
+
 
 class MainScreen(BaseScreen):
     def main_title_is_exist(self):
